e8c9f02278142fcd905839992ffb83ece8afd8ba
MatsRivel <mats-r@hotmail.com>
2023-10-31 10:58:16 +0100

    Finished 2020_4. Finished 2020_5, but overwrote it

--- AdventOfCode_2020/day4/src/p2.rs
+++ AdventOfCode_2020/day4/src/p2.rs
@@ -1,16 +1,6 @@
 use std::{fs::read_to_string, string, collections::HashMap};
 use crate::p1::{self, passport_batch};
 
-#[derive(Debug)]
-struct Passport{
-    byr: u32,
-    ecl: String,
-    eyr: u32,
-    hcl: String,
-    hgt: String,
-    iyr: u32,
-    pid: String,
-}
 fn is_present(sorted_fields: &Vec<&str>)->bool{
     let mut correct_fields = ["byr", "iyr", "eyr", "hgt", "hcl","ecl","pid"];
     correct_fields.sort();
@@ -21,17 +11,16 @@ fn is_present(sorted_fields: &Vec<&str>)->bool{
     }
     true
 }
-
 fn byr(s:&str)->bool{
     let val = match s.parse::<u32>(){
         Ok(v) => Some(v),
         Err(_) => {
-            println!("Invalid byr!: {}",s);
+            // println!("Invalid byr!: {}",s);
             None
         }
     };
     if let Some(byr) = val{
-        if byr >= 1920 || byr <= 2002{
+        if byr >= 1920 && byr <= 2002{
             return true;
         }
     }
@@ -51,7 +40,7 @@ fn eyr(s:&str)->bool{
             return false;
         }
     };
-    if eyr >= 2020 || eyr <= 2030{
+    if eyr >= 2020 && eyr <= 2030{
         return true;
     }
     false
@@ -60,16 +49,20 @@ fn hcl(s:&str)->bool{
     let hcl = s.to_string();
     if let Some(colour) = hcl.strip_prefix("#"){
         if colour.len() != 6{
+            // println!("Haircolour {s} has too few chars after \"#\"");
             return false;
         }
         // Must consist of only these characters:
         for c in colour.chars(){
             if !['a','b','c','d','e','f'].contains(&c) && ! ['0','1','2','3','4','5','6','7','8','9'].contains(&c){
+                // println!("Haircolour {s} has invalid chars after \"#\"");
                 return false;
             }
         }
-    }else{ // Haircolour has to start with a "#"
-        return false;
+        return true; // Valid haricolour
+    }else{
+        // Haircolour has to start with a "#"
+        false
     }
 }
 fn hgt(s:&str)->bool{
@@ -77,16 +70,21 @@ fn hgt(s:&str)->bool{
     if let Some(height_string) = hgt.strip_suffix("cm"){
         let height_in_cm = height_string.parse::<u32>().unwrap();
         if height_in_cm < 150 || height_in_cm > 193{
+            // println!("Height {s} is out of range!");
             return false;
         }
     }else if let Some(height_string) = hgt.strip_suffix("in"){
         let height_in_inch = height_string.parse::<u32>().unwrap();
         if height_in_inch < 59 || height_in_inch > 76{
+            // println!("Height {s} is out of range!");
             return false;
         }
-    }else{ // Return false if height is neither cm or inches.
-        return false;
+    }else{
+        // Return false if height is neither cm or inches.
+        // println!("Height {s} is not cm or inches!");
+        return false
     }
+    true
 }
 fn iyr(s:&str)->bool{
     let iyr = match s.parse::<u32>(){
@@ -95,7 +93,7 @@ fn iyr(s:&str)->bool{
             return false;
         }
     };
-    if iyr < 2010 || self.iyr > 2020{
+    if iyr < 2010 || iyr > 2020{
         return false;
     }
     true
@@ -107,8 +105,8 @@ fn pid(s:&str)->bool{
     }
     true
 }
-fn get_components(string:String) -> Option<Vec<[String;2]>>{
-    let mut components = string.split(" ").enumerate().filter_map(|(i,s)|{
+fn get_components(string:&str) -> Option<Vec<[String;2]>>{
+    let mut components = string.split(" ").filter_map(|s|{
         if s == ""{
             None
         }else{
@@ -134,15 +132,38 @@ fn get_components(string:String) -> Option<Vec<[String;2]>>{
     }).collect::<Vec<[String;2]>>();
     Some(new_components)
 }
+fn field_triage(field:&str, component:&str)->bool{
+    match field{
+        "byr" => byr(component),
+        "ecl" => ecl(component),
+        "eyr" => eyr(component),
+        "hcl" => hcl(component),
+        "hgt" => hgt(component),
+        "iyr" => iyr(component),
+        "pid" => pid(component),
+        "cid" => true, // "Cid is always fine :)"
+        _ => panic!("Unexpected field! >> {field} <<") 
+    }
+}
+fn valid_passport(passport:&Vec<[String;2]>) -> bool{
+    for [field, component] in passport.iter() {
+        if !field_triage(&field, &component){
+            // println!("Failed!: Field: {field}, component: {component}");
+            return false;
+        }
 
+    }
+    // println!("Valid passport! {passport:?}");
+    true
+}
 pub fn main_2(file_name:&str)->Option<i32>{
     let data_string = read_to_string(file_name).unwrap();
     let mut batch = passport_batch(&data_string);
     let passports = batch.iter().filter_map(|b|{
-        get_components(*b)
+        get_components(b.as_str())
     }).collect::<Vec<Vec<[String;2]>>>();
     let n_valid_passports = passports.iter().fold(0, |acc, passport| {
-        if passport.is_valid(){
+        if valid_passport(passport){
             acc+1
         }else{
             acc
---
+++ AdventOfCode_2020/day5/README.txt
@@ -0,0 +1,5 @@
+I completed this puzzle.
+I accidentally overwrote the current code with the new template.
+I pressed ctrl+z to undo.
+It undid the paste, but did not return the old files.
+I can not be bothered to redo it, as the task was fairly simple.
