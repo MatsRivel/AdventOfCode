1ad5d4ba71500933859f3fef2acdca559a8c32d7
MatsRivel <65452175+MatsRivel@users.noreply.github.com>
2023-12-10 22:49:40 +0100

    Initialized 2023_10

---
+++ AdventOfCode_2023/day10/src/p1.rs
@@ -0,0 +1,24 @@
+use std::fs::read_to_string;
+pub fn main_1(file_name:&str)->Option<i32>{
+
+    /*
+    Plan:
+    Define loop. (Continue until you find an existing node?)
+    Follow loop all the way through.
+    Cut that distance in half.
+    Done.
+     */
+    None
+
+}
+
+#[cfg(test)]
+    mod tests{
+    use super::*;
+
+    #[test]
+    fn my_test(){
+
+    }
+
+}
